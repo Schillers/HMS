6c415b2e7afb689f8466f6fbdd2547d8ad08ebcf
Schillers <36090281+Schillers@users.noreply.github.com>
2018-02-19 09:51:49 +0000

    Add files via upload

---
+++ build-management-server.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+source ./managementserver/installmrf.sh
+source ./managementserver/installmhn.sh
+
+#-----------------------------------------------
+#
+# Show title banner
+#
+#-----------------------------------------------
+echo -e "#########################################################"
+echo -e "##                                                     ##"
+echo -e "##          H.M.S - Honeynet Management Suite          ##"
+echo -e "##                                                     ##"
+echo -e "##           https://github.com/Schillers/HMS          ##"
+echo -e "##                                                     ##"
+echo -e "#########################################################"
+sleep 5
+
+#-----------------------------------------------
+#
+# Checking is user is running as root
+#
+#-----------------------------------------------
+echo -e "\n\n### Checking if running as root user... ###"
+if [ "$EUID" -ne 0 ]
+  then read -n 1 -s -r -p "Please run as root. Press any key to continue..."
+  clear
+  exit 1
+else
+  echo -e "Check complete..."
+fi
+
+#-----------------------------------------------
+#
+# Prepare the system for install
+#
+#-----------------------------------------------
+sleep 2
+echo -e "\n\n### Updating your system and preparing for install ###"
+sleep 5
+apt-get update
+apt-get install nano git -y
+
+sleep 2
+read -r -p "Would you like to install the Malware Repository Framework (MRF)? [Y/n] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY]|)+$ ]]
+then
+    installmrf
+else
+    :
+fi
+
+sleep 2
+read -r -p "### Would you like to install the Modern Honey Network (MHN)? ### [Y/n] " response
+if [[ "$response" =~ ^([yY][eE][sS]|[yY]|)+$ ]]
+then
+    installmhn
+else
+    :
+fi
+
+# TODO
+# Incorporate Server variable like server name and email address.
